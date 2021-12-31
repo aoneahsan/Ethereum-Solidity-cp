aec433ca670b85ecd412ed33d9ae72ba2fcd141e
Ahsan Mahmood <aoneahsan@gmail.com>
2021-12-31 16:45:45 +0500

    fix: updated

--- deploy.js
+++ deploy.js
@@ -16,42 +16,44 @@ const web3ObjInstance = new Web3Class(providerOptions)
 const deployContract = async () => {
   try {
     // const contracts = [CompaignCompiledContract, CompaignFactorCompiledContract]
-    const contracts = [CompaignFactorCompiledContract] // we don't need to deploy the compaign contract as we will use factory contract to later deploy compaigns
-    for (let i = 0; i < contracts.length; i++) {
-      const {
-        abi: interface,
-        evm: {
-          bytecode: { object: bytecode }
-        }
-      } = contracts[i]
-      const accounts = await web3ObjInstance.eth.getAccounts()
-      const result = await new web3ObjInstance.eth.Contract(interface)
-        .deploy({
-          data: bytecode
-        })
-        .send({ gas: '10000000', from: accounts[0] })
-      const contractAddress = result.options.address
-      console.log('interface S...', i)
-      console.log(interface)
-      console.log('interface E...', i)
-
-      console.log('contractAddress S...', i)
-      console.log({ contractAddress })
-      console.log('contractAddress E...', i)
-      const dataToStore = {
-        interface,
-        bytecode,
-        contractAddress
+    // const contracts = [CompaignFactorCompiledContract] // we don't need to deploy the compaign contract as we will use factory contract to later deploy compaigns
+    // for (let i = 0; i < contracts.length; i++) {
+    const {
+      abi: interface,
+      evm: {
+        bytecode: { object: bytecode }
       }
+    } = CompaignFactorCompiledContract
+    const accounts = await web3ObjInstance.eth.getAccounts()
+    const result = await new web3ObjInstance.eth.Contract(interface)
+      .deploy({
+        data: bytecode
+      })
+      .send({ gas: '10000000', from: accounts[0] })
+    const contractAddress = result.options.address
+    console.log('interface S...')
+    console.log(interface)
+    console.log('interface E...')
 
-      fs.writeJSONSync(
-        path.resolve(
-          __dirname,
-          `./deployed-contracts/deployment-output-${new Date().toLocaleDateString()}.json`
-        ),
-        dataToStore
-      )
+    console.log('contractAddress S...')
+    console.log({ contractAddress })
+    console.log('contractAddress E...')
+    const dataToStore = {
+      interface,
+      bytecode,
+      contractAddress
     }
+
+    fs.ensureDirSync(path.resolve(__dirname, `./deployed-contracts`))
+
+    fs.outputJSONSync(
+      path.resolve(
+        __dirname,
+        `./deployed-contracts/deployment-output-${new Date().toISOString()}.json`
+      ),
+      dataToStore
+    )
+    // }
   } catch (error) {
     console.error('Error in deploy.js Start----')
     console.log(error)
